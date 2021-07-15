d90584d41bf3a59c96445a193cbb59c7588a0222
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-07-15 12:02:06 +0200

    Install styled components, add global CSS reset styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-
 import { fetchAccount } from './features/auth/authSlice';
+
+import CSSReset from './Reset.jsx';
 
 store.dispatch(fetchAccount());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <CSSReset />
       <App />
     </Provider>
   </React.StrictMode>,
