6a81b550f2a11947a715ad2943989a53a0740226
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-07-19 14:15:39 +0200

    Modify backend to include a count of all docs for users

--- util/paginateMongoose.js
+++ util/paginateMongoose.js
@@ -3,19 +3,35 @@ const createError = require('http-errors');
 module.exports = function paginate(mongooseModel, entriesPerPage = 10) {
   if (mongooseModel == null) throw new Error('Model is undefined/null.');
 
-  return (req, res, next) => {
+  return async (req, res, next) => {
     let { page = 1 } = req.query;
     page = parseInt(page);
 
     if (Number.isNaN(page) || page <= 0)
       return next(createError(400, 'Invalid page number.'));
 
-    mongooseModel
-      .find({})
-      .sort({ createdAt: -1 })
-      .skip(entriesPerPage * (page - 1))
-      .limit(entriesPerPage)
-      .then((docs) => res.json(docs))
-      .catch((err) => next(err));
+    let count = 0;
+    try {
+      count = await mongooseModel.countDocuments({});
+    } catch (err) {
+      next(err);
+    }
+
+    let docs = [];
+    try {
+      docs = await mongooseModel
+        .find({})
+        .sort({ createdAt: -1 })
+        .skip(entriesPerPage * (page - 1))
+        .limit(entriesPerPage);
+    } catch (err) {
+      next(err);
+    }
+
+    res.json({
+      docs,
+      maxQuota: count,
+      remainingQuota: count - entriesPerPage,
+    });
   };
 };
