992bcc4bb281256a27657d04e1fa90bb96010e79
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-08-11 13:08:47 +0200

    Fix navbar not being responsive on lower viewports

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import UsersIcon from '../assets/icons/users.svg';
 import ProfileIcon from '../assets/icons/profile.svg';
 import HomeIcon from '../assets/icons/home.svg';
+import LogoutIcon from '../assets/icons/logout.svg';
 
 let Wrapper = styled.div`
   background-color: hsl(204, 86%, 46%);
@@ -12,26 +13,6 @@ let Wrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 0 2rem;
-
-  & > a {
-    text-decoration: none;
-    font-size: 1.4rem;
-
-    background-color: white;
-    color: hsl(0, 0%, 7%);
-
-    width: max-content;
-    height: fit-content;
-    padding: 0.7rem;
-    border-radius: 5px;
-
-    display: grid;
-    place-items: center;
-  }
-
-  & > a:hover {
-    background-color: hsl(0, 0%, 90%);
-  }
 `;
 
 let Logo = styled.h1`
@@ -39,6 +20,10 @@ let Logo = styled.h1`
 
   background-color: white;
   border-radius: 5px;
+
+  @media only screen and (max-width: 450px) {
+    font-size: 1.5rem;
+  }
 `;
 
 let NavigationBox = styled.div`
@@ -55,6 +40,29 @@ let PageTitle = styled.h2`
   color: white;
 `;
 
+let LogoutButton = () => {
+  let StyledImg = styled.img`
+    height: 2.8rem;
+    padding: 0 0.5rem;
+
+    border-radius: 5px;
+
+    background-color: white;
+
+    &:hover {
+      background-color: hsl(0, 0%, 90%);
+
+      cursor: pointer;
+    }
+
+    @media only screen and (max-width: 450px) {
+      height: 1.5rem;
+    }
+  `;
+
+  return <StyledImg src={LogoutIcon} />;
+};
+
 let NavigationIcon = styled.img`
   height: 2.2rem;
   padding: 0 0.5rem;
@@ -67,6 +75,7 @@ let NavigationIcon = styled.img`
     cursor: pointer;
   }
 `;
+
 let Tooltipped = styled.span`
   position: relative;
 
@@ -124,7 +133,11 @@ function Navbar({ isLoggedIn, title }) {
             </div>
             <PageTitle>{title}</PageTitle>
           </NavigationBox>
-          <a href="/api/auth/logout">Logout</a>
+          <a href="/api/auth/logout">
+            <Tooltipped text="Logout">
+              <LogoutButton />
+            </Tooltipped>
+          </a>
         </>
       )}
     </Wrapper>
