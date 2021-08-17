ba9e54a0b7cf597d6e53669a05f8dca4e41627fb
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-08-17 13:23:10 +0200

    Group remove/edit post buttons into a single options dropdown

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import DeleteIcon from '../assets/icons/delete.svg';
 import EditIcon from '../assets/icons/edit.svg';
+import OptionsIcon from '../assets/icons/options.svg';
 
 // Redux
 import { useSelector } from 'react-redux';
@@ -11,38 +12,120 @@ import axios from 'axios';
 
 import PostForm from './PostForm';
 import Modal from './Modal';
+import styled from 'styled-components';
+
+let Dropdown = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  width: max-content;
+  overflow: hidden;
+
+  background: white;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 100%);
+
+  z-index: 50;
+`;
+
+let DropdownButton = styled.button`
+  background: none;
+  border: none;
+
+  padding: 5px 10px;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+  }
+`;
+
+let RelativeContainer = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+let PostContainer = styled.div`
+  width: 100%;
+  max-width: 500px;
+
+  margin: 0 auto;
+`;
+
+let Icon = styled.img`
+  width: 1.8rem;
+  height: 1.8rem;
+`;
+
+let OptionsButton = styled.button`
+  background: none;
+  border: none;
+
+  border-radius: 100%;
+
+  width: 1.8rem;
+  height: 1.8rem;
+
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
 
 function Post({ data, onDelete }) {
   let auth = useSelector(selectCurrentUser);
   let [editMode, setEditMode] = useState(false);
   let [text, setText] = useState(data.text);
   let [displayDeleteModal, setDisplayDeleteModal] = useState(false);
+  let [displayDropdown, setDisplayDropdown] = useState(false);
 
   function DropdownMenu() {
     return (
-      <div>
-        <button
+      <RelativeContainer>
+        <OptionsButton
           onClick={() => {
-            setDisplayDeleteModal(true);
+            setDisplayDropdown((oldState) => !oldState);
           }}
         >
-          <img src={DeleteIcon} alt="Delete Button" />
-          Remove
-        </button>
-        <button
-          onClick={() => {
-            setEditMode(true);
-          }}
-        >
-          <img src={EditIcon} alt="Edit Button" />
-          Edit
-        </button>
-      </div>
+          <Icon src={OptionsIcon} alt="Options Button" />
+        </OptionsButton>
+
+        {displayDropdown && (
+          <Dropdown>
+            <DropdownButton
+              onClick={() => {
+                setDisplayDeleteModal(true);
+              }}
+            >
+              <img src={DeleteIcon} alt="Delete Button" />
+              Remove
+            </DropdownButton>
+            <DropdownButton
+              onClick={() => {
+                setEditMode(true);
+              }}
+            >
+              <img src={EditIcon} alt="Edit Button" />
+              Edit
+            </DropdownButton>
+          </Dropdown>
+        )}
+      </RelativeContainer>
     );
   }
 
   return (
-    <div>
+    <PostContainer>
       <Modal
         onConfirm={() => {
           axios.delete(`/api/post/${data._id.toString()}`).then(() => {
@@ -79,7 +162,7 @@ function Post({ data, onDelete }) {
           <br />
         </>
       )}
-    </div>
+    </PostContainer>
   );
 }
 
