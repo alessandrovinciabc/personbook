95e972eed058720524092e7a879dbc37b88f18ad
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-08-24 14:40:30 +0200

    Improve styles for New Post Form: hover, proper size

--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -5,6 +5,53 @@ import { fetchAccount } from '../features/auth/authSlice';
 
 import axios from 'axios';
 
+import styled from 'styled-components';
+
+let PostTextArea = styled.textarea`
+  resize: none;
+  width: 100%;
+  max-width: 400px;
+  height: 100px;
+
+  font-family: inherit;
+`;
+
+let PostButton = styled.button`
+  width: 100%;
+  max-width: 400px;
+
+  height: 2rem;
+
+  margin-top: 10px;
+
+  background-color: #1089da;
+  color: white;
+  border: none;
+  border-radius: 5px;
+
+  cursor: pointer;
+
+  &:hover {
+    filter: opacity(0.8);
+  }
+
+  &:disabled {
+    background-color: rgba(0, 0, 0, 0.2);
+    filter: opacity(1);
+
+    cursor: inherit;
+  }
+`;
+
+let Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
 function PostForm({ onConfirm, onCancel, post }) {
   let [text, setText] = useState(post?.text || '');
 
@@ -27,21 +74,21 @@ function PostForm({ onConfirm, onCancel, post }) {
   }
 
   return (
-    <div>
-      <textarea
+    <Container>
+      <PostTextArea
         type="text"
         value={text}
         maxLength="1024"
         onChange={(e) => {
           setText(e.target.value);
         }}
-      ></textarea>
+      ></PostTextArea>
       <br />
-      <button disabled={text.trim() === ''} onClick={handlePost}>
+      <PostButton disabled={text.trim() === ''} onClick={handlePost}>
         {postId ? 'Confirm' : 'Post'}
-      </button>
+      </PostButton>
       {postId && <button onClick={onCancel}>Cancel</button>}
-    </div>
+    </Container>
   );
 }
 
